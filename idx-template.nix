77abd1b54327d058e5bd7021b6bbeebca36f654c
samphillips <samphillips@google.com>
2025-03-11 15:37:48 +0000

    Update template nix

--- idx-template.nix
+++ idx-template.nix
@@ -1,31 +1,10 @@
-{ pkgs, version ? "latest", importAlias ? "@/*", language ? "ts"
-, packageManager ? "npm", srcDir ? false, eslint ? false, app ? false
-, tailwind ? false, ... }: {
-
-  packages = [ pkgs.nodejs_20 pkgs.yarn pkgs.nodePackages.pnpm pkgs.bun ];
-
-  bootstrap = ''
-    			mkdir "$out"
-    			npx create-next-app@${version} "$out" \
-					  --yes \
-					  --skip-install \
-    				--import-alias=${importAlias} \
-    				--${language} \
-    				--use-${packageManager} \
-    				${if eslint then "--eslint" else "--no-eslint"} \
-    				${if srcDir then "--src-dir" else "--no-src-dir"} \
-    				${if app then "--app" else "--no-app"} \
-    				${if tailwind then "--tailwind" else "--no-tailwind"}
-
-    			mkdir -p "$out"/.idx
-      		cp ${./dev.nix} "$out"/.idx/dev.nix
-    			chmod -R +w "$out"
-
-    			${
-         if packageManager == "npm" then
-           "( cd $out && npm i --package-lock-only --ignore-scripts )"
-         else
-           ""
-       }
-  '';
+{pkgs, ...}: {
+    packages = [
+        pkgs.nodejs
+    ];
+    bootstrap = ''
+        mkdir "$out"
+        cp -rf ${./}/. "$out"
+        chmod -R u+w "$out"
+    '';
 }
