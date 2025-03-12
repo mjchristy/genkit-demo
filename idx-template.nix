e91e9d6064928051c8548ecb2b99e073f0805694
samphillips <samphillips@google.com>
2025-03-12 02:30:09 +0000

    fixup

--- idx-template.nix
+++ idx-template.nix
@@ -4,9 +4,9 @@
     ];
     bootstrap = ''
 		cp -rf ${./.}/. "$WS_NAME"
-    sed -i "s/REPLACE_ME_GOOGLE_GENAI_API_KEY/${apiKey}/g" "$WS_NAME"/.idx/dev.nix
 		chmod -R +w "$WS_NAME"
 
 		mv "$WS_NAME" "$out"
+    sed -i "s/REPLACE_ME_GOOGLE_GENAI_API_KEY/${apiKey}/g" "$out"/.idx/dev.nix
     '';
 }
