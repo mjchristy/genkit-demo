778c11ef6dc88da099aedbb39d675223266c5829
samphillips <samphillips@google.com>
2025-03-12 02:25:36 +0000

    fixup

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -10,7 +10,7 @@
  
    env = {
     #TODO Get a API key from https://g.co/ai/idxGetGeminiKey 
-    GOOGLE_GENAI_API_KEY = ""; 
+    GOOGLE_GENAI_API_KEY = "REPLACE_ME_GOOGLE_GENAI_API_KEY"; 
   };
 
   # Workspace lifecycle hooks
--- idx-template.nix
+++ idx-template.nix
@@ -1,10 +1,12 @@
-{pkgs, ...}: {
+{pkgs, apiKey, ...}: {
     packages = [
         pkgs.nodejs
     ];
     bootstrap = ''
 		cp -rf ${./.}/. "$WS_NAME"
+    sed -i "s/REPLACE_ME_GOOGLE_GENAI_API_KEY/${apiKey}/g" "$WS_NAME"/.idx/dev.nix
 		chmod -R +w "$WS_NAME"
+
 		mv "$WS_NAME" "$out"
     '';
 }
